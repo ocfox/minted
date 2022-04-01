224fa1d6ccdffce0a621cd08bcc501c596f7f568
ocfox <i@ocfox.me>
2022-04-01 23:35:44 +0800

    feat/object: add velocity and drop judge

--- index.js
+++ index.js
@@ -7,25 +7,60 @@ canvas.height = 576
 context.fillRect(0, 0, canvas.width, canvas.height)
 
 class Sprite {
-  constructor(position) {
+  constructor({ position, velocity }) {
     this.position = position
+    this.velocity = velocity
   }
 
   draw() {
     context.fillStyle = 'red'
     context.fillRect(this.position.x, this.position.y, 50, 150)
   }
+
+  update() {
+    if (this.position.y + 150 + this.velocity.y >= canvas.height) {
+      this.velocity.y = 0
+      this.position.y = canvas.height - 150
+    }
+    this.draw()
+    this.position.x += this.velocity.x
+    this.position.y += this.velocity.y
+  }
 }
 
 const player = new Sprite({
-  x: 0,
-  y: 0
+  position: {
+    x: 300,
+    y: 0
+  },
+  velocity: {
+    x: 0,
+    y: 10
+  }
 })
 
 const enemy = new Sprite({
-  x: 600,
-  y: 200
+  position: {
+    x: 700,
+    y: 400
+  },
+  velocity: {
+    x: 0,
+    y: 10
+  }
 })
+
+
+
+function animate() {
+  window.requestAnimationFrame(animate)
+  context.fillStyle = 'black'
+  context.fillRect(0, 0, canvas.width, canvas.height)
+  player.update()
+  enemy.update()
+}
+
+animate()
 
 player.draw()
 enemy.draw()
